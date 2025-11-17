8ba779e6e2b0a48243e482d9d1450b270e8ffa2b
sumpumm <samarpan69.piya@gmail.com>
2025-11-17 21:59:45 +0545

    created embeddings and performed similarity search

---
+++ main.py
@@ -0,0 +1,25 @@
+from sentence_transformers import SentenceTransformer,util
+import json
+
+model = SentenceTransformer("all-MiniLM-L6-v2")
+
+user_question="tell me your name?"
+user_question_embedding=model.encode(user_question)
+
+with open("FAQ.json","r") as f:
+    data = json.load(f)
+
+
+
+emb=model.encode(data["questions"])
+cos_sim=util.cos_sim(emb,user_question_embedding)
+
+id=0
+q_id=0
+for x in cos_sim:
+    if x > 0.7:
+        q_id=id
+    id+=1
+
+print("The most similar sentence matching the user question is:")
+print(data["questions"][q_id])
