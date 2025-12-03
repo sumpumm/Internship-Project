a380f09c68de1e26e70d82458c985437c5622c00
sumpumm <samarpan69.piya@gmail.com>
2025-12-03 07:17:09 +0545

    successfully integrated gemini

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 from google import genai
-import os
+import os,json
 from dotenv import load_dotenv
+from sentence_transformers import SentenceTransformer,util
 
 load_dotenv()
+#LOADING REQUIRED MODELS
 client = genai.Client(api_key=os.getenv('API_KEY'))
-
-from sentence_transformers import SentenceTransformer,util
-import json
-
 model = SentenceTransformer("all-MiniLM-L6-v2")
 
-user_question="tell me your name?"
+user_question="what is the color of the sky?"
 user_question_embedding=model.encode(user_question)
 
 with open("FAQ.json","r") as f:
@@ -23,10 +21,20 @@ cos_sim=util.cos_sim(emb,user_question_embedding)
 
 id=0
 q_id=0
+flag= False
+
 for x in cos_sim:
     if x > 0.7:
         q_id=id
+        flag=True
     id+=1
 
-print("The most similar sentence matching the user question is:")
-print(data["questions"][q_id])
+if not flag:
+    response=client.models.generate_content(
+        model="gemini-2.0-flash",
+        contents=user_question
+    )
+    print(response.text)
+else:
+    print("Answer:")
+    print(data["answers"][q_id])
